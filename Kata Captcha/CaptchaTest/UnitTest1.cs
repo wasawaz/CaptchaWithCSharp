9ee7700826f3fdc6a852fed836d6a423203753e3
Wasawat <wasawaz@gmail.com>
2015-05-11 12:31:52 +0700

    Add Unit Test

---
+++ Kata Captcha/CaptchaTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CaptchaTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestFail()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
